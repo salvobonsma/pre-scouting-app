b6b819776252c13971271ea92d22279d18bc4c83
ampersanded <salvobonsma@gmail.com>
2024-04-19 20:13:05 -0700

    init

---
+++ app/error.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import {useEffect} from 'react'
+import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
+export default function Error({error, reset}: { error: Error & { digest?: string }, reset: () => void }) {
+    useEffect(() => {
+        console.error(error)
+    }, [error])
+
+    return (
+          <div className={"h-screen flex justify-center items-center"}>
+              <Card>
+                  <CardHeader>
+                      <CardTitle>Error</CardTitle>
+                      <CardDescription>Something when wrong.</CardDescription>
+                  </CardHeader>
+                  <CardFooter className={"flex justify-end"}>
+                      <Button onClick={() => reset}>Try Again</Button>
+                  </CardFooter>
+              </Card>
+          </div>
+    )
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
+export default function NotFound() {
+    return (
+          <div className={"h-screen flex justify-center items-center"}>
+              <Card>
+                  <CardHeader>
+                      <CardTitle>Not Found</CardTitle>
+                      <CardDescription>Could not find what you were looking for.</CardDescription>
+                  </CardHeader>
+                  <CardFooter className={"flex justify-between"}>
+                      <Link href={"/dashboard"}><Button className={"mr-6"}>Go to Dashboard</Button></Link>
+                      <Link href={"/"}><Button>Return Home</Button></Link>
+                  </CardFooter>
+              </Card>
+          </div>
+    )
+}
